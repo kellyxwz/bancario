da3c5f34de51a6b76d2d6c62e95f8fd8da2825bc
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-06-14 16:28:47 -0300

    feat: created class TokenService

---
+++ src/main/java/com/sistema/bancario/infra/security/TokenService.java
@@ -0,0 +1,53 @@
+package com.sistema.bancario.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.sistema.bancario.entities.User;
+import org.hibernate.sql.Insert;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(getExpirationDate())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException e){
+            throw  new RuntimeException("Error while generating token", e);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth.api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return "";
+        }
+    }
+
+    private Instant getExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
