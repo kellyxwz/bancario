b06af9faa66f5d62d54991ed2283deb408fc7748
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-06-17 13:52:42 -0300

    feat: Add validations using Spring Validation to the RequestTransactionDTO

--- src/main/java/com/sistema/bancario/DTO/Request/RequestTransactionDTO.java
+++ src/main/java/com/sistema/bancario/DTO/Request/RequestTransactionDTO.java
@@ -1,11 +1,18 @@
 package com.sistema.bancario.DTO.Request;
 
 import com.sistema.bancario.entities.enums.TransactionType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 
 public record RequestTransactionDTO(
+
+        @NotBlank(message = "The transaction amount is required")
+        @Positive(message = "The transaction value must be greater than zero")
         BigDecimal value,
+
+        @NotBlank(message = "The transaction type is required")
         TransactionType type
 )
 {
