707f423fd619b9df6f32abcb414b1a97345a7c40
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-06-17 13:46:52 -0300

    feat: Add validations using Spring Validation to the RequestUserDT

--- src/main/java/com/sistema/bancario/DTO/Request/RequestUserDTO.java
+++ src/main/java/com/sistema/bancario/DTO/Request/RequestUserDTO.java
@@ -1,9 +1,20 @@
 package com.sistema.bancario.DTO.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record RequestUserDTO(
-        String name,
+        @NotBlank(message = "Name is required") String name,
+
+        @NotBlank(message = "Email is required")
+        @Email( message = "Email invalid")
         String email,
+
         Long phone,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, message = "The password must have minimum 6 characters")
         String password
 ) {
 }
