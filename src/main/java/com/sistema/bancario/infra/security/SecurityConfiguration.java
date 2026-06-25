e98bde357ba1da8d6cbb0ea97b69ea5ef9853eb3
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-06-25 19:29:19 -0300

    feat: create Exceptions Path

--- src/main/java/com/sistema/bancario/infra/security/SecurityConfiguration.java
+++ src/main/java/com/sistema/bancario/infra/security/SecurityConfiguration.java
@@ -29,9 +29,13 @@ public class SecurityConfiguration {
        return httpSecurity.csrf(csrf -> csrf.disable())
                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                .authorizeHttpRequests(authorize -> authorize
-                       .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                       .requestMatchers(HttpMethod.POST, "/auth/login").hasRole("CLIENT")
                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                       .requestMatchers(HttpMethod.POST, "/transaction/**").permitAll()
+
+                       .requestMatchers(HttpMethod.POST, "/transaction/**").hasRole("CLIENT")
+
+                       .requestMatchers(HttpMethod.POST, "/account/user/**").permitAll()
+                       .requestMatchers(HttpMethod.GET, "/account/**").hasRole("CLIENT")
 
                        .requestMatchers(HttpMethod.GET, "/users/**").authenticated()
                        .requestMatchers(HttpMethod.PUT, "/users/**").hasRole("CLIENT")
