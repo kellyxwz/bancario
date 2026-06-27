0c8a0b759c615c13ba09ddce720485b687a7f195
kellyxwz <kellyramosdeoliveira25@gmail.com>
2026-06-26 22:51:35 -0300

    create class ResorceNotFound

---
+++ src/main/java/com/sistema/bancario/exceptions/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.sistema.bancario.exceptions;
+
+public class ResourceNotFound {
+
+
+
+}
---
+++ src/main/java/com/sistema/bancario/exceptions/globalExceptionError.java
@@ -0,0 +1,10 @@
+package com.sistema.bancario.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class globalExceptionError {
+
+
+
+}
